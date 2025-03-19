6c5ac6b1e524c6f04605adfcbde5b9d92c757637
wilfredkiprop <kipropwilfred90@gmail.com>
2025-03-19 13:23:25 +0300

    domcontentloaded lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+   let paragraph= document.getElementById("text");
+   paragraph.textContent="This is really cool!"
+
+});
